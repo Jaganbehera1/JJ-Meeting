e0787b2154652379b2a8a7a5e1cce4f943591553
Jaganbehera1 <jaganbehera63@gmail.com>
2025-10-06 17:29:17 +0530

    first commit

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
-// Firebase configuration from environment variables
+// Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAgZCtcnltf6um5felvWP3r_L1rJt3dEgQ",
+    authDomain: "online-classes-83846.firebaseapp.com",
+    databaseURL: "https://online-classes-83846-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "online-classes-83846",
+    storageBucket: "online-classes-83846.firebasestorage.app",
+    messagingSenderId: "187976015161",
+    appId: "1:187976015161:web:9b44253a575b011e835ce8",
+    measurementId: "G-8600VC5E8Q"
 };
 
 // Initialize Firebase
